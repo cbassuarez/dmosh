c7913900e1f2b2b0cd5500fa810b0438eeba01b2
Seb Suarez <contact@cbassuarez.com>
2025-12-05 19:58:37 -0800

    fix: tests

--- tests/vitestEnvironment.ts
+++ tests/vitestEnvironment.ts
@@ -1,33 +1,33 @@
-import { JSDOMEnvironment } from "vitest/environments/jsdom"
+import { JSDOMEnvironment } from "vitest/environments"
 import type { Environment } from "vitest"
 
 function ensureBufferFlags() {
-  const resizableDescriptor = Object.getOwnPropertyDescriptor(ArrayBuffer.prototype, "resizable")
-  if (!resizableDescriptor || typeof resizableDescriptor.get !== "function") {
-    Object.defineProperty(ArrayBuffer.prototype, "resizable", {
-      get() {
-        return false
-      },
-      configurable: true,
-    })
-  }
+const resizableDescriptor = Object.getOwnPropertyDescriptor(ArrayBuffer.prototype, "resizable")
+if (!resizableDescriptor || typeof resizableDescriptor.get !== "function") {
+Object.defineProperty(ArrayBuffer.prototype, "resizable", {
+get() {
+return false
+},
+configurable: true,
+})
+}
 
-  if (typeof SharedArrayBuffer !== "undefined") {
-    const sabGrowableDescriptor = Object.getOwnPropertyDescriptor(SharedArrayBuffer.prototype, "growable")
-    if (!sabGrowableDescriptor || typeof sabGrowableDescriptor.get !== "function") {
-      Object.defineProperty(SharedArrayBuffer.prototype, "growable", {
-        get() {
-          return false
-        },
-        configurable: true,
-      })
-    }
-  }
+if (typeof SharedArrayBuffer !== "undefined") {
+const sabGrowableDescriptor = Object.getOwnPropertyDescriptor(SharedArrayBuffer.prototype, "growable")
+if (!sabGrowableDescriptor || typeof sabGrowableDescriptor.get !== "function") {
+Object.defineProperty(SharedArrayBuffer.prototype, "growable", {
+get() {
+return false
+},
+configurable: true,
+})
+}
+}
 }
 
 export default class PatchedJsdomEnvironment extends JSDOMEnvironment implements Environment {
-  constructor(options: any) {
-    ensureBufferFlags()
-    super(options)
-  }
+constructor(options: ConstructorParameters<typeof JSDOMEnvironment>[0]) {
+ensureBufferFlags()
+super(options)
+}
 }
